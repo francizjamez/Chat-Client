e1340af5f9509dfe6ea09062fbf41651721e5de2
Francis James Dela Cruz <56282809+francizjamez@users.noreply.github.com>
2021-02-02 19:08:17 +0800

    Update client.js

--- client.js
+++ client.js
@@ -1,33 +1,63 @@
 const io = require("socket.io-client");
 const readline = require('readline');
-const rl = readline.createInterface({ input: process.stdin,  output: process.stdout });
+const { response } = require("express");
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 rl.question('Enter your username:', (name) => {
 
     console.log(`Welcome ${name}`);
-    
-    const socket = io('http://localhost:3000');
-    let eventName = "simple chat message";
 
-    let chat = function(){
-        rl.question("> ", (msg) =>{
-            console.log(`Sending message: ${msg}`);
-            socket.emit(eventName, `${name}: ${msg}`);
-            chat();
+    const socket = io('http://localhost:3000');
+    let eventName = "response";
+
+    let play = function () {
+        rl.question("(R)ock, (P)aper or (S)cissors?", (choice) => {
+            
+            switch(choice){
+                case "p": choice = "paper";break;
+                case "s": choice = "scissors";break;
+                case "r": choice = "rock";break;
+            }
+            console.log(`You chose ${choice}`);
+            console.log(`Waiting for response`);
+            socket.emit(eventName, [name, choice]);
         })
     }
 
-    socket.on('connect',  () =>{
+    socket.on('connect', () => {
         console.log("Connected to server");
-        chat();
+        play();
     });
 
     socket.on('disconnect', () => {
         console.log("Disconnected from server")
     })
 
-    socket.on(eventName, msg => {
-        console.log(msg);
-        chat();
+    socket.on("output", ([responses, output]) => {
+
+        let enemy = responses[0][0] === name ? responses[1] : responses[0];
+
+
+        switch (output) {
+            case "draw":
+                console.log(`${enemy[0]} chose ${enemy[1]} - ${output}`); break;
+            case "p1":
+                if (responses[0][0] === name) {
+                    console.log(`${enemy[0]} chose ${enemy[1]} - You win`);
+                }
+                else{
+                    console.log(`${enemy[0]} chose ${enemy[1]} - You lose`);
+                }
+                break;
+            case "p2":
+                if (responses[0][0] === name) {
+                    console.log(`${enemy[0]} chose ${enemy[1]} - You lose`);
+                }
+                else{
+                    console.log(`${enemy[0]} chose ${enemy[1]} - You win`);
+                }
+                break;
+        }
+        play();
     });
 });
